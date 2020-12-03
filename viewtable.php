f30d6087b7c350fbe19b707aee1eed2f9927249a
pangyuantong <pangyuantong@users.noreply.github.com>
2020-12-03 12:40:17 +0800

    Update viewtable.php

--- viewtable.php
+++ viewtable.php
@@ -19,6 +19,37 @@
 <th>Menara Standard Chartered, TPM</th>
 <th>03-22441234</th>
 </tr>
+<?php
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "simpleweb", "pwd" => "Simple@web", "Database" => "simpleweb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:simplewebtp045550.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+if (!$conn)
+{
+die("Error connection: ".sqlsrv_errors());
+}
+else{
+    echo"<script>alert('Success!');</script>";
+}
+
+$tsql= "SELECT * FROM [dbo].[restaurant]";
+$getResults= sqlsrv_query($conn, $tsql);
+if ($getResults == FALSE)
+{
+die(sqlsrv_errors());
+}
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
+{
+echo "<tr>";
+echo "<td>". $row['restaurant_id'] . "</td>";
+echo "<td>". $row['restaurant_name'] ."</td>";
+echo "<td>". $row['restaurant_address'] . "</td>";
+echo "<td>". $row['restaurant_phone'] . "</td>";
+echo "</tr>";
+}
+sqlsrv_free_stmt($getResults);
+?>
 </table>
 </center>
 </body>
